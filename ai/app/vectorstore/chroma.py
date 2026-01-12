65fecc8eeef00aa64d8efada5ea26edfc23a97d2
2005LIKHITH <likhithcvsrl@gmail.com>
2026-01-12 14:29:18 +0530

    ft:fixed folder structure

--- ai/app/vectorstore/chroma.py
+++ ai/app/vectorstore/chroma.py
@@ -1,6 +1,7 @@
 import chromadb
 from app.config import VECTOR_DB_PATH, COLLECTION_NAME
 
-client = chromadb.PersistentClient(path=VECTOR_DB_PATH)
+# Connect to Dockerized Chroma (exposed on localhost:8001)
+client = chromadb.HttpClient(host='localhost', port=8001)
 
 collection = client.get_or_create_collection(COLLECTION_NAME)
